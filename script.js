f96a710bcbf0d1acaab79d579e345b18e640450d
João Matheus Jinkings <joaomatheusjinkings@gmail.com>
2023-08-31 17:07:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-/* Desenvolva seu código abaixo */
+function playRockPaperScissor (player1, player2) {
+
+    const rock = "Pedra";
+    const paper = "Papel";
+    const scissor = "Tesoura";
+
+    if((player1 === rock && player2 === scissor)||(player1 === paper && player2 === rock)||(player1 === scissor && player2 === paper)) {
+        return "Jogador 1 venceu!";
+    }
+    else if((player1 === rock && player2 === paper)||(player1 === paper && player2 === scissor)||(player1 === scissor && player2 === rock)) {
+        return "Jogador 2 venceu!";
+    }
+    else if((player1 === rock && player2 === rock)||(player1 === paper && player2 === paper)||(player1 === scissor && player2 === scissor)) {
+        return "Empate!"
+    }
+}
+
+    
+
+    
+    
+
+
